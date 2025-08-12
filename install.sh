891bc9e63bb6998e32528998a9f7df7cf3bbbc87
Sebastien Roche <sroche@g.harvard.edu>
2025-08-12 13:47:06 -0400

    updated install to use environment-py312.yml

--- install.sh
+++ install.sh
@@ -56,10 +56,10 @@ done
 
 if $env_exists; then
     echo "$envname already exists, will update installed packages"
-    conda env update --name "$envname" --file environment-py310.yml
+    conda env update --name "$envname" --file environment-py312.yml
 else
     echo "Will create conda environment '$envname'"
-    conda env create --name "$envname" --file environment-py310.yml
+    conda env create --name "$envname" --file environment-py312.yml
 fi
 
 
